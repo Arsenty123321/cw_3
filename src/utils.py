12cbc46f49365f88b189a13b1fcb1d07e502906b
Arsenty Dafintsev <s_pool@mail.ru>
2024-05-13 01:51:48 +0300

    Add get_executed_operations func

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,10 @@
 import json
 from pathlib import Path
 
+from typing import List
 
-def load_json_data(path: Path):
+
+def load_json_data(path: Path) -> List[dict]:
     """
     :param path: Путь к файлу с данными
     :return: Возвращает словарь с данными
@@ -10,3 +12,11 @@ def load_json_data(path: Path):
 
     with open(path, encoding='UTF-8') as file:
         return json.load(file)
+
+
+def get_executed_operations(operations: List[dict]):
+    """
+    :param operations: Словарь с данными операций
+    :return: Возвращает словарь с executed операциями
+    """
+    return [operation for operation in operations if operation.get("state") == "EXECUTED"]
